ed613900717782e4fecabbe55656838b6dbac4dc
Jackson Otto <jacksonotto120@gmail.com>
2025-05-27 00:52:56 -0700

    update color scheme options

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,36 @@
-return {
-  "thesimonho/kanagawa-paper.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("kanagawa-paper").setup({
-      transparent = true,
-      colors = {
-        palette = {
-          sumiInk6 = '#828282',
-        }
-      },
-    })
-    vim.opt.termguicolors = true
-    vim.cmd.colorscheme("kanagawa-paper")
-  end,
-}
+local setColorScheme = function(name)
+  vim.cmd.colorscheme(name)
+end
+
+local colorschemes = require("utils.colorschemes").colorschemes
+
+local active = "kanagawa"
+
+local res = {}
+
+for i = 1, table.getn(colorschemes) do
+  local item = colorschemes[i]
+  local priority = 0
+
+  if item.name == active then
+    priority = 1000
+  end
+
+  res[i] = {
+    item[1],
+    lazy = item.name ~= active,
+    priority = priority,
+    enable = item.name == active,
+    config = function()
+      if item.name ~= active then
+        return
+      end
+
+      item.config()
+      vim.opt.termguicolors = true
+      setColorScheme(item.name)
+    end
+  }
+end
+
+return res
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
   config = function()
     local ts_status_ok, treesitter_configs = pcall(require, "nvim-treesitter.configs")
     if not ts_status_ok then
@@ -27,24 +28,28 @@ return {
     })
 
     treesitter_configs.setup({
-      modules = {},
+      -- modules = {},
 
       -- A list of parser names, or "all"
-      ensure_installed = {},
+      ensure_installed = {
+        "javascript",
+        "typescript",
+        "tsx",
+      },
 
       -- List of parsers to ignore installing (for "all")
-      ignore_install = {},
+      -- ignore_install = {},
 
       -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
+      -- sync_install = false,
 
       -- Automatically install missing parsers when entering buffer
       auto_install = true,
 
-      matchup = {
-        enable = false, -- mandatory, false will disable the whole extension
-        -- disable = { "c", "ruby" },  -- optional, list of language that will be disabled
-      },
+      -- matchup = {
+      --   enable = false, -- mandatory, false will disable the whole extension
+      --   -- disable = { "c", "ruby" },  -- optional, list of language that will be disabled
+      -- },
       highlight = {
         enable = true, -- false will disable the whole extension
         additional_vim_regex_highlighting = false,
@@ -57,46 +62,49 @@ return {
           return big_file_status_ok and big_file_detected
         end,
       },
-      indent = { enable = true, disable = { "yaml", "python" } },
+      indent = {
+        enable = true,
+        disable = { "yaml", "python" },
+      },
       autotag = { enable = false },
-      textobjects = {
-        swap = {
-          enable = false,
-          -- swap_next = textobj_swap_keymaps,
-        },
-        -- move = textobj_move_keymaps,
-        select = {
-          enable = false,
-          -- keymaps = textobj_sel_keymaps,
-        },
-      },
-      textsubjects = {
-        enable = false,
-        keymaps = { ["."] = "textsubjects-smart", [";"] = "textsubjects-big" },
-      },
-      playground = {
-        enable = false,
-        disable = {},
-        updatetime = 25,         -- Debounced time for highlighting nodes in the playground from source code
-        persist_queries = false, -- Whether the query persists across vim sessions
-        keybindings = {
-          toggle_query_editor = "o",
-          toggle_hl_groups = "i",
-          toggle_injected_languages = "t",
-          toggle_anonymous_nodes = "a",
-          toggle_language_display = "I",
-          focus_language = "f",
-          unfocus_language = "F",
-          update = "R",
-          goto_node = "<cr>",
-          show_help = "?",
-        },
-      },
-      rainbow = {
-        enable = false,
-        extended_mode = true,  -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
-        max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
-      },
+      -- textobjects = {
+      --   swap = {
+      --     enable = false,
+      --     -- swap_next = textobj_swap_keymaps,
+      --   },
+      --   -- move = textobj_move_keymaps,
+      --   select = {
+      --     enable = false,
+      --     -- keymaps = textobj_sel_keymaps,
+      --   },
+      -- },
+      -- textsubjects = {
+      --   enable = false,
+      --   keymaps = { ["."] = "textsubjects-smart", [";"] = "textsubjects-big" },
+      -- },
+      -- playground = {
+      --   enable = false,
+      --   disable = {},
+      --   updatetime = 25,         -- Debounced time for highlighting nodes in the playground from source code
+      --   persist_queries = false, -- Whether the query persists across vim sessions
+      --   keybindings = {
+      --     toggle_query_editor = "o",
+      --     toggle_hl_groups = "i",
+      --     toggle_injected_languages = "t",
+      --     toggle_anonymous_nodes = "a",
+      --     toggle_language_display = "I",
+      --     focus_language = "f",
+      --     unfocus_language = "F",
+      --     update = "R",
+      --     goto_node = "<cr>",
+      --     show_help = "?",
+      --   },
+      -- },
+      -- rainbow = {
+      --   enable = false,
+      --   extended_mode = true,  -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
+      --   max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
+      -- },
     })
 
     local ts_utils = require("nvim-treesitter.ts_utils")
