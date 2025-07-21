93b633993975e6be9a27c991bf2499c42a7f0400
Jackson Otto <jacksonotto120@gmail.com>
2025-07-21 12:54:35 -0600

    move insert ai keybinds to init function

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -95,6 +95,4 @@ load_maps()
 vim.keymap.set({ "n", "v" }, "L", "<Nop>", opts)
 vim.keymap.set({ "n", "v", "i" }, '<F1>', '<Nop>', opts)
 
--- custom plugins
-vim.keymap.set('n', '<leader>p', ':lua require("insert-ai-nvim").run_inference()<CR>', opts)
-vim.keymap.set('n', '<leader>ao', ':lua require("insert-ai-nvim").show_prev_output()<CR>', opts)
+require("insert-ai-nvim").init()
