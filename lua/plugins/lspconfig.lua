b45b3d3f767dcf842de4cc93a7713e934401c5cb
Jackson Otto <jacksonotto120@gmail.com>
2026-02-03 20:11:14 -0800

    fix border problem

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -30,23 +30,22 @@ return {
 
             -- lsp borders
             local border_style = "rounded"
-            vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-                border = border_style
-            })
+            vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+                vim.lsp.handlers.hover,
+                { border = border_style }
+            )
             vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
                 vim.lsp.handlers.signature_help,
                 { border = border_style }
             )
             vim.diagnostic.config({
                 virtual_text = true,
-                float = {
-                    border = border_style,
-                },
+                float = { border = border_style, },
             })
             vim.o.winborder = "rounded"
 
             require("lspconfig.ui.windows").default_options = {
-                border = border_style,
+                border = border_style
             }
 
             vim.api.nvim_win_set_option(
@@ -57,7 +56,7 @@ return {
             local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
             function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
                 opts = opts or {}
-                opts.border = opts.border or "rounded"
+                opts.border = "rounded"
                 return orig_util_open_floating_preview(contents, syntax, opts, ...)
             end
         end
