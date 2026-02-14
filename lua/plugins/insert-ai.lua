9764af83842a1c3e6ceff303a716185a2aec5d2b
Jackson Otto <jacksonotto120@gmail.com>
2026-02-13 18:59:44 -0800

    remove insert ai plugin (never used it lmao)

--- lua/plugins/insert-ai.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "JacksonO123/insert-ai-nvim",
-    event = "VeryLazy",
-    config = function()
-        local insert_ai = require("insert-ai-nvim")
-        insert_ai.init()
-    end
-}
