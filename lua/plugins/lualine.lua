c902f8b3a4076d954ca5b1a09dad4b264b7391e4
Jackson Otto <jacksonotto120@gmail.com>
2026-02-17 12:48:48 -0800

    update lualine config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,6 +1,22 @@
 return {
     "nvim-lualine/lualine.nvim",
     config = function()
-        require("lualine").setup {}
+        local custom_theme = require('lualine.themes.auto') -- or any other theme like 'vscode', 'gruvbox', etc.
+        custom_theme.normal.c.bg = 'None'
+        custom_theme.inactive.c.bg = 'None'
+
+        require('lualine').setup({
+            options = {
+                theme = custom_theme,
+            },
+            sections = {
+                lualine_x = {
+                    'encoding',
+                    'filetype',
+                },
+            },
+        })
+
+        vim.cmd("hi StatusLine guibg=NONE")
     end
 }
