895f16cb40355aca376f9b21f15d41094d1d10ec
Jackson Otto <jacksonotto120@gmail.com>
2026-02-17 13:03:15 -0800

    update lualine config to actually be good

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,9 +1,18 @@
 return {
     "nvim-lualine/lualine.nvim",
     config = function()
+        vim.cmd("hi StatusLine guibg=NONE")
+        vim.cmd("hi StatusLineNC guibg=NONE")
+
         local custom_theme = require('lualine.themes.auto')
-        custom_theme.normal.c.bg = 'None'
-        custom_theme.inactive.c.bg = 'None'
+
+        custom_theme.normal.c.bg = "NONE"
+        custom_theme.insert.c.bg = "NONE"
+        custom_theme.replace.c.bg = "NONE"
+        custom_theme.visual.c.bg = "NONE"
+        custom_theme.command.c.bg = "NONE"
+        custom_theme.inactive.c.bg = "NONE"
+        custom_theme.terminal.c.bg = "NONE"
 
         require('lualine').setup({
             options = {
@@ -16,7 +25,5 @@ return {
                 },
             },
         })
-
-        vim.cmd("hi StatusLine guibg=NONE")
     end
 }
