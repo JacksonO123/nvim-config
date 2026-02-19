74a905d454556440c3a2360389ce2ebdf6052775
Jackson Otto <jacksonotto120@gmail.com>
2026-02-18 23:35:10 -0800

    add specific root markers to nvim-eslint

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -9,11 +9,41 @@ return {
         lazy = true,
         event = "VeryLazy",
         config = function()
+            local settings = require("config.settings");
+
             require("nvim-eslint").setup({
+                root_dir = function(bufname)
+                    local root_markers = {
+                        ".git",
+                        "eslint.config.js",
+                        "eslint.config.mjs",
+                        "eslint.config.cjs",
+                        "eslint.config.ts",
+                        "eslint.config.mts",
+                        "eslint.config.cts",
+                        ".eslintrc.js",
+                        ".eslintrc.cjs",
+                        ".eslintrc.yaml",
+                        ".eslintrc.yml",
+                        ".eslintrc.json",
+                        ".eslintrc",
+                    }
+
+                    local found = vim.fs.find(root_markers, {
+                        upward = true,
+                        path = vim.fs.dirname(bufname)
+                    })
+
+                    if found[1] then
+                        return vim.fs.dirname(found[1])
+                    end
+
+                    return nil
+                end,
                 settings = {
                     workingDirectory = { mode = "location" },
                     run = "onType",
-                    format = true,
+                    format = settings.format_on_save,
                     codeActionOnSave = {
                         enable = true,
                         mode = "all",
