19b5c207565207d4c17ea17fba5b8fa5e4fd7c41
Jackson Otto <jacksonotto120@gmail.com>
2026-02-19 11:50:37 -0800

    remove unecessary root_dir config

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -12,34 +12,6 @@ return {
             local settings = require("config.settings");
 
             require("nvim-eslint").setup({
-                root_dir = function(bufname)
-                    local root_markers = {
-                        ".git",
-                        "eslint.config.js",
-                        "eslint.config.mjs",
-                        "eslint.config.cjs",
-                        "eslint.config.ts",
-                        "eslint.config.mts",
-                        "eslint.config.cts",
-                        ".eslintrc.js",
-                        ".eslintrc.cjs",
-                        ".eslintrc.yaml",
-                        ".eslintrc.yml",
-                        ".eslintrc.json",
-                        ".eslintrc",
-                    }
-
-                    local found = vim.fs.find(root_markers, {
-                        upward = true,
-                        path = vim.fs.dirname(bufname)
-                    })
-
-                    if found[1] then
-                        return vim.fs.dirname(found[1])
-                    end
-
-                    return nil
-                end,
                 settings = {
                     workingDirectory = { mode = "location" },
                     run = "onType",
