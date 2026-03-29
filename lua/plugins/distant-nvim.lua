66ba6339ce1cd89efed28efbd3bbad47165ae498
Jackson Otto <jacksonotto120@gmail.com>
2026-03-29 13:02:53 -0700

    fix distant nvim not underlining lsp errors or warnings etc

--- lua/plugins/distant-nvim.lua
+++ lua/plugins/distant-nvim.lua
@@ -1,8 +1,13 @@
 return {
     'chipsenkbeil/distant.nvim',
     branch = 'v0.3',
-    enabled = false,
     config = function()
         require('distant'):setup()
+
+        -- distant nvim bug chipsenkbeil/distant.nvim#125 globally disables diagnostic underlines.
+        -- so, re-enable them
+        vim.schedule(function()
+            vim.diagnostic.config({ underline = true, signs = true })
+        end)
     end
 }
