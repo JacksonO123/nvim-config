6ad8332757715858b51f10eb40d6955e8df72c80
Jackson Otto <jacksonotto120@gmail.com>
2026-04-13 00:27:07 -0700

    add exclude files snacks picker

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -11,22 +11,46 @@ return {
                 keys = {
                     { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
                     { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-                    { icon = " ", key = "s", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-                    { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-                    { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+                    {
+                        icon = " ",
+                        key = "s",
+                        desc = "Find Text",
+                        action = ":lua Snacks.dashboard.pick('live_grep')",
+                    },
+                    {
+                        icon = " ",
+                        key = "r",
+                        desc = "Recent Files",
+                        action = ":lua Snacks.dashboard.pick('oldfiles')",
+                    },
+                    {
+                        icon = " ",
+                        key = "c",
+                        desc = "Config",
+                        action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+                    },
                     { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-                    { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+                    {
+                        icon = "󰒲 ",
+                        key = "L",
+                        desc = "Lazy",
+                        action = ":Lazy",
+                        enabled = package.loaded.lazy ~= nil,
+                    },
                     { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-                }
-            }
+                },
+            },
         },
         explorer = { enabled = false },
         indent = { enabled = false },
         input = { enabled = false },
         picker = {
+            ignored = true,
+            hidden = true,
+            exclude = { "**/.git", "**/.DS_Store", "**/node_modules", "**/.zig-cache" },
             enabled = true,
             layout = {
-                preset = "telescope"
+                preset = "telescope",
             },
         },
         notifier = { enabled = false },
