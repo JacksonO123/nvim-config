631851da43be1f1b12cf045ca7c5183dee22afab
Jackson Otto <jacksonotto120@gmail.com>
2026-07-17 23:53:47 -0700

    add zig capabilities to lsp config

--- lua/lsp/zls.lua
+++ lua/lsp/zls.lua
@@ -1,3 +1,7 @@
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
 return {
     cmd = { vim.fn.expand("~/.local/bin/zls") },
+    capabilities = capabilities,
 }
